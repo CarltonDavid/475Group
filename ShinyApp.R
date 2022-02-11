c35df168fa090fb9a9139535790ce859b3287678
Spencer Wood <Swood30@vols.utk.edu>
2022-02-11 14:51:47 -0500

    Autoplot
    Added Autoplot time series

--- ShinyApp.R
+++ ShinyApp.R
@@ -1,10 +1,19 @@
 library(shiny)
+library(fpp3)
+data(tourism)
 
 ui <- fluidPage(
+  mainPanel(plotOutput("Plot"))
   
 )
 
 server <- function(input, output, session) {
+  output$Plot <- renderPlot({
+    CV <- filter(tourism, Region == "Clare Valley")
+    autoplot(CV)
+    
+  })
+  
   
 }
 
